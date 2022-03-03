00468d9e89794f39041e18a66c3ca0fde8c562fc
Carlos Agut <carlosagutvives@gmail.com>
2022-03-03 11:27:32 +0100

    PR modularidad

---
+++ Angular/Proyecto1/01-bases/src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Ironman', 'Spiderman', 'Hulk', 'Thor'];
+  heroeBorrado: string = '';
+  
+  borrarHeroes():string[]{
+    this.heroeBorrado = this.heroes.shift() || '';
+    return this.heroes;
+  }
+
+}
